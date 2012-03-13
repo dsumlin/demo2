323d6aeb917ecd68179407836c7ebbb3dc1613cb
dsumlin <webmaster@dennissumlin.com>
2012-03-12 23:05:58 -0400

    modAppHelp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
-#Return a titl
+#Return a title
 
 def title 
 base_title = "Dennis on Ruby"
-if @titl.nil?
+if @title.nil?
 base_title
 else
 "#{base_title} | #{@title}"
