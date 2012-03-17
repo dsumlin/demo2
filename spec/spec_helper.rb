251995ed8d548ac809dde4b2032e062216ad6888
dsumlin <webmaster@dennissumlin.com>
2012-03-17 17:10:09 -0400

    d

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'factory_girl'
-require 'factory_girl_rails'
+
 
 
 Factory.find_definitions
