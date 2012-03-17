20cef2379f01021239eac462733172a979145b61
dsumlin <webmaster@dennissumlin.com>
2012-03-17 16:25:13 -0400

    bunt out

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,10 @@
 module UsersHelper
     
-   def gravatar_for(user)
+    def gravatar_for(user, options = { :size => 50})
        
-       gravatar_image_tag(user.email.downcase)
+        gravatar_image_tag(user.email.downcase, :alt => user.name,
+                                                :class => 'gravatar',
+                                                :gravatar => options )
        
        end
     
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl'
+require 'factory_girl_rails'
+
+
 Factory.find_definitions
 
 # Requires supporting ruby files with custom matchers and macros, etc,
