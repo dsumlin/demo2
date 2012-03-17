363c6868e6635534dd1dd39d44b9c17bb31e9de0
dsumlin <webmaster@dennissumlin.com>
2012-03-17 15:20:38 -0400

    factory first step

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
     
-    attr_accessible :name, :email
+    
+    attr_accessor   :password
+    attr_accessible :name, :email, :password_confirmation
     
     email_regex = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
     
@@ -10,5 +12,57 @@ class User < ActiveRecord::Base
                             :format => { :with => email_regex },
                             :uniqueness => { :case_sensitive => false}
 
+    
+    
+    
+    before_save :encrypt_password
+    
+    def has_password?(submitted_password)
+        
+        encrypted_password == encrypt(password)
+        
+       
+    end
+    
+    
+    class << self do
+    
+    def authenticate(email, submitted_password)
+    
+        user = find_by_email(email)
+        return nil if user.nil?
+        return user if user.has_password?(submitted_password)
+        
+    end
+end
+
+        
+    
+    
+    private
+    
+        def encrypt_password
+            self.salt = make_salt if new_record?
+            self.encrypt_password = encrypt(password)
+            end
+    
+    def encrypt(string)
+        
+        secure_hash("#{salt}--#{string}")
+        
+        end
+    
+    def make_salt
+        
+        secure_hash("#{Time.now.utc}--#{password}")
+        
+    end
+    
+    def secure_hash(string)
+        
+        Diges::SHA2.hesdigest(string)
+        
+        end
+    
     
     end
