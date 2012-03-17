4a6b8585923dadf310dc758ec28c10db2a7a9eb7
dsumlin <webmaster@dennissumlin.com>
2012-03-17 13:31:59 -0400

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ActiveRecord::Base
     
     attr_accessible :name, :email
     
+    email_regex = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
     
-end
+    validates :name,        :presence => true,
+                            :length => { :maximum => 50 }
+    validates :email,       :presence => true,
+                            :format => { :with => email_regex },
+                            :uniqueness => true
+
+    
+    end
