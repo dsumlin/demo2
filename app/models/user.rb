d3abaea6e63adf3199ac28b80bb0e4f14cea4666
dsumlin <webmaster@dennissumlin.com>
2012-03-17 21:23:05 -0400

    help

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     
     
     attr_accessor   :password
-    attr_accessible :name, :email, :password_confirmation, :admin
+    attr_accessible :name, :email, :password, :password_confirmation
     
     email_regex = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
     
