69140fe440721e3f7cb1007e24b5ac9685591e98
dsumlin <webmaster@dennissumlin.com>
2012-03-18 01:05:01 -0400

    l

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+      @title = "Sign in"
+  end
+    
+    def create
+        
+        render 'new'
+        
+    end
+    
+    def destroy
+    end
+    
+    end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Demo2::Application.routes.draw do
   
+  get "sessions/new"
+    
+
     resources :users
+    resources :sessions, :only => [:new, :create, :destroy]
     
   get "users/new"
 
@@ -11,7 +15,10 @@ Demo2::Application.routes.draw do
     match '/about', :to => 'pages#about'
     match '/home', :to => 'pages#home'
     match '/help', :to => 'pages#help'
-    match '/signup', :to => 'users#new' 
+    match '/signup', :to => 'users#new'
+    match '/signin', :to => 'sessions#new' 
+    match '/signout', :to => 'sessions#destroy' 
+
     match '/show', :to => 'users#show'
 
     
