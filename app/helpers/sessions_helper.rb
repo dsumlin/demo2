5586205df4380ac69c4a91a3b3908f8ebc7378a0
dsumlin <webmaster@dennissumlin.com>
2012-03-20 13:01:57 -0400

    break

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
     def sign_in(user)
     
         cookies.permanent.signed[:remember_token] = [user.id, user.salt]   
-        @current_user = user
+        current_user = user
     end
     
     
@@ -43,10 +43,9 @@ module SessionsHelper
         
         end
     
+    end
     
     
-    
-end
 
 
 
