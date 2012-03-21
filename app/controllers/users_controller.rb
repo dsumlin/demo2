1cfade8edfeea7b1930259e50b06a778df38f983
dsumlin <webmaster@dennissumlin.com>
2012-03-20 23:33:25 -0400

    duck again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,5 +34,15 @@ class UsersController < ApplicationController
         @title = "Edit user"
         
         end
+    
+    def update
+        @user = User.find(params[:id])
+            if @user.update_attributes(params[:user])
+                redirect_to @user, :flash => { :success => "Updated" }
+            else
+        @title = "Edit user"
+        render 'edit'
+        end
+        end
             
 end
