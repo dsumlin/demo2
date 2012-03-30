a08f23fed3833f9092d8e0ec82378c4bc78ab4ae
dsumlin <webmaster@dennissumlin.com>
2012-03-30 01:06:04 -0400

    hh

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ group :assets do
 end
 
 gem 'jquery-rails'
-
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 require 'will_paginate'
+config.gem 'paperclip', :source => 'http://rubygems.org'
+
+
 # Initialize the rails application
 Demo2::Application.initialize!
