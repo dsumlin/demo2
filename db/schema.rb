4bdc0e83b735153562c851a4628eebe947cfa48e
dsumlin <webmaster@dennissumlin.com>
2012-04-07 12:58:22 -0400

    second

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20120329044718) do
     t.datetime "updated_at", :null => false
   end
 
+  add_index "blogposts", ["user_id"], :name => "index_blogposts_on_user_id"
+
   create_table "friendships", :force => true do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
@@ -52,11 +54,11 @@ ActiveRecord::Schema.define(:version => 20120329044718) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.string   "salt"
     t.string   "encrypted_password"
-    t.boolean  "admin"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
