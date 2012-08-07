ac01adfa2a543ff21d4b66a71bf9edf523de127a
dsumlin <webmaster@dennissumlin.com>
2012-08-06 22:02:21 -0400

    h

---
+++ app/assets/javascripts/myJs.js
@@ -0,0 +1,13 @@
+function message() {
+
+alert("You are now following a new memeber! Please Refresh Page to Save!")
+
+
+}
+
+function message2() {
+
+alert("You are now unfollowing a memeber! Please Refresh Page to Save!")
+
+
+}
